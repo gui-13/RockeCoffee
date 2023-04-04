4776bb3d8343987aadce641744b3fc59e1edfb8a
Guilherme Prudente <guilhermepbelmiro@gmail.com>
2023-04-03 23:04:14 -0300

    first commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
 export function Button() {
   return (
-    <button className="uppercase px-7 py-2 border-borderButton border-2 rounded-lg hover:border-purple-500 cursor-pointer">
+    <button className="uppercase px-7 py-2 sm:my-0 my-10 border-borderButton border-2 rounded-lg hover:border-purple-500 cursor-pointer">
       pegar meu café
     </button>
   )
--- src/components/TitleMobile.jsx
+++ src/components/TitleMobile.jsx
@@ -5,7 +5,7 @@ const styles = {
 export function TitleMobile() {
   return (
     <div className=" flex flex-col items-center">
-      <h1 className="font-bold text-5xl text-center mt-10">Great Coffee</h1>
+      <h1 className="font-bold text-5xl text-center">Great Coffee</h1>
       <h1 style={styles} className="font-bold text-5xl text-center text-black"> &lt; Great Code /&gt;</h1>
     </div>
   )
